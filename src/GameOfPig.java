714ccf44b115d56ad7c60dcf5c5408218a710a54
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-08-06 10:50:00 -0400

    Initial commit

---
+++ src/GameOfPig.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameOfPig {
+
+	public static void main(String[] args) {
+		
+		 JFrame pigFrame = new JFrame("Pig Game");;
+		 pigFrame.setPreferredSize(new Dimension(400, 500));
+
+		 pigFrame.pack();
+		 pigFrame.setVisible(true);
+
+		 
+
+		}
+	
+}
