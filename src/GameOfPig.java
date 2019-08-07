6055230755ffb2da10a86ffec84718ed480b14d3
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-08-07 13:00:01 -0400

    Frame, panel and button added. no functionality added yet

--- src/GameOfPig.java
+++ src/GameOfPig.java
@@ -1,19 +1,45 @@
-import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class GameOfPig {
+
+public class GameOfPig extends JFrame{
 
 	public static void main(String[] args) {
 		
-		 JFrame pigFrame = new JFrame("Pig Game");
-		 pigFrame.setPreferredSize(new Dimension(400, 500));
-
-		 pigFrame.pack();
-		 pigFrame.setVisible(true);
-
+		new GameOfPig();
 		 
-
-		}
+	}
 	
+	public GameOfPig() {
+
+		super("Pig Game");
+
+		JButton rollDiceButton = new JButton("Roll Dice"); 
+		JLabel mainFrameLabel = new JLabel();
+		
+		
+		add(rollDiceButton);
+		rollDiceButton.setBounds(100, 100, 100, 80);
+		add(mainFrameLabel, BorderLayout.CENTER);
+		super.setSize(700, 800); 
+
+		setVisible(true);
+				
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		this.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent e) {
+				dispose();        		 
+			}
+		});
+
+		
+	}
+
 }
